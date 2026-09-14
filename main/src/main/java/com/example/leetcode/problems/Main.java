dce8dd54f5d0874037fbf37200fe2617de71a9c7
rish1299 <rishabhrao2468@gmail.com>
2026-09-14 21:16:23 +0530

    Implement TwoSum class and update Main to demonstrate its usage

--- main/src/main/java/com/example/leetcode/problems/Main.java
+++ main/src/main/java/com/example/leetcode/problems/Main.java
@@ -1,7 +1,14 @@
 package com.example.leetcode.problems;
 
+import com.example.leetcode.problems.TwoSum.TwoSum;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        TwoSum solution = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
     }
 }
